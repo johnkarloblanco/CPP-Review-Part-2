0d44ab3f5908d3eeeccb86314febcd93bd6435b7
johnBlanco <johnkarlo.blanco@yahoo.ca>
2020-06-15 18:08:26 -0600

    Add project files.

---
+++ CPP-Review-Part-2/Source.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+
+//accessing and creating text files
+
+int main()
+{
+	ifstream inStream;
+	inStream.open("passphrase.txt");
+	ofstream outStream;
+	outStream.open("NewPassPhrase.txt", ios::app);
+
+	int userGuess = 0;
+	string passPhrase;
+
+	if (!inStream.fail())
+	{
+		while (inStream >> passPhrase)
+		{
+			cout << "The pass phrase is: " << passPhrase << endl;
+			cout << "What is your answer?\n";
+			cin >> userGuess;
+			if (userGuess == passPhrase.length())
+			{
+				cout << "Congrats\n";
+			}
+			else
+			{
+				cout << "Sorry try again later\n";
+				outStream << passPhrase << endl;
+			}
+		}
+	}
+	inStream.close();
+	outStream.close();
+
+}
